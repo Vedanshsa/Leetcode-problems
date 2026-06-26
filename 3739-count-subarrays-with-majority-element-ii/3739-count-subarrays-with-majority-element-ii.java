429f20d27fd99e604a5da6f3fda7a5c3d3a45973
Vedansh Saini <136168816+Vedanshsa@users.noreply.github.com>
2026-06-26 23:12:56 +0530

    Time: 54 ms (26.23%), Space: 122.2 MB (14.76%) - LeetHub

---
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+class Solution {
+    public long countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        int[] arr = new int[n];
+        boolean found = false;
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == target) {
+                arr[i] = 1;
+                found = true;
+            } else {
+                arr[i] = -1;
+            }
+        }
+        
+        if (!found) return 0;
+        
+        
+        long[] pref = new long[n + 1];
+        pref[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            pref[i] = pref[i - 1] + arr[i - 1];
+        }
+        
+        
+        long[] sorted = pref.clone();
+        Arrays.sort(sorted);
+        Map<Long, Integer> comp = new HashMap<>();
+        int idx = 1;
+        for (long v : sorted) {
+            if (!comp.containsKey(v)) {
+                comp.put(v, idx++);
+            }
+        }
+        
+        
+        Fenwick fenwick = new Fenwick(idx + 2);
+        long ans = 0;
+        
+        for (int i = 0; i <= n; i++) {
+            int pos = comp.get(pref[i]);
+            ans += fenwick.query(pos - 1); 
+            fenwick.update(pos, 1);
+        }
+        
+        return ans;
+    }
+    
+    static class Fenwick {
+        long[] bit;
+        int n;
+        Fenwick(int n) {
+            this.n = n;
+            bit = new long[n + 1];
+        }
+        void update(int i, long val) {
+            while (i <= n) {
+                bit[i] += val;
+                i += i & -i;
+            }
+        }
+        long query(int i) {
+            long sum = 0;
+            while (i > 0) {
+                sum += bit[i];
+                i -= i & -i;
+            }
+            return sum;
+        }
+    }
+}
