b054f7e70d1ccc7924ba655c9545890e3c4fb743
Vedansh Saini <136168816+Vedanshsa@users.noreply.github.com>
2026-07-26 15:43:47 +0530

    Time: 2 ms (99.61%), Space: 47 MB (97.34%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,16 +1,17 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+
         for (int x : nums) {
-            maxHeap.offer(x);
-            minHeap.offer(x);
+            if (x > max1) { max3 = max2; max2 = max1; max1 = x; }
+            else if (x > max2) { max3 = max2; max2 = x; }
+            else if (x > max3) { max3 = x; }
+
+            if (x < min1) { min2 = min1; min1 = x; }
+            else if (x < min2) { min2 = x; }
         }
-        
-        int max1 = maxHeap.poll(), max2 = maxHeap.poll(), max3 = maxHeap.poll();
-        int min1 = minHeap.poll(), min2 = minHeap.poll();
-        
+
         return Math.max(max1 * max2 * max3, min1 * min2 * max1);
     }
 }
