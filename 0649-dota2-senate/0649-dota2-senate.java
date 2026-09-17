aeecfdf9d9c693a431fa625ef7b38bca117f4d63
Vedansh Saini <136168816+Vedanshsa@users.noreply.github.com>
2026-09-17 16:20:39 +0530

    Time: 10 ms (90.1%), Space: 47.2 MB (46.17%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public String predictPartyVictory(String senate) {
+        Queue<Integer> radiant = new LinkedList<>();
+        Queue<Integer> dire = new LinkedList<>();
+        int n = senate.length();
+
+        for (int i = 0; i < n; i++) {
+            if (senate.charAt(i) == 'R') {
+                radiant.add(i);
+            } else {
+                dire.add(i);
+            }
+        }
+
+        while (!radiant.isEmpty() && !dire.isEmpty()) {
+            int r = radiant.poll();
+            int d = dire.poll();
+            if (r < d) {
+                radiant.add(r + n); // Radiant bans Dire, Radiant survives to next round
+            } else {
+                dire.add(d + n);    // Dire bans Radiant, Dire survives to next round
+            }
+        }
+
+        return radiant.isEmpty() ? "Dire" : "Radiant";
+    }
+}
