8d85f13b703dbb90f2cc1d7f2c685714f8215263
Vedansh Saini <136168816+Vedanshsa@users.noreply.github.com>
2026-09-17 16:11:09 +0530

    Time: 5 ms (64.5%), Space: 47.2 MB (28.04%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int a : asteroids) {
+            boolean alive = true;
+            while (alive && !stack.isEmpty() && stack.peek() > 0 && a < 0) {
+                if (stack.peek() < -a) {
+                    stack.pop();
+                } else if (stack.peek() == -a) {
+                    stack.pop();
+                    alive = false;
+                } else {
+                    alive = false;
+                }
+            }
+            if (alive) {
+                stack.push(a);
+            }
+        }
+        
+        int[] result = new int[stack.size()];
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+        return result;
+    }
+}
